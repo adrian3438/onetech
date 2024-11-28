82748d6e48eeb61e70df7beab92ba64c169b1d41
adrian3438 <admin@bmcnet.kr>
2024-11-28 10:06:44 +0900

    main page Update due to none used component

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,5 +1,4 @@
 import '../../asset/css/page.scss'
-import Image from "next/image";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import EquipmentContent from "../../components/EquipmentContent";
