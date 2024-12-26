81ad387622d3ddd7493f27b971562fb407632feb
adrian3438 <admin@bmcnet.kr>
2024-12-26 13:48:12 +0900

    사업분야 및 제조장비 수정

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     //baseURL : 'http://dkloknext.localhost/api/controller',
-    baseURL : 'https://marineplaza.org/valmax-api/controller',
+    baseURL : 'https://marineplaza.org/onetech-api/controller',
 })
 
 export default instance;
