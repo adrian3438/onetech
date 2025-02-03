0e608a9667a5b1e0509bf5f4a54297a337056628
jdlee <jdlee@bmcnet.kr>
2025-02-03 10:17:49 +0900

    - 오류 수정 진행중(블로그 영역 주석 처리)

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import EquipmentContent from "../../components/EquipmentContent";
 //import Facilities from "../../components/Facilities";
-import Blog from "../../components/Blog";
+// import Blog from "../../components/Blog";
 import ContactUs from "../../components/ContactUs";
 import BusinessDivision from "../../components/BusinessDivision";
 import CompanyHistory from "../../components/CompanyHistory";
@@ -32,7 +32,7 @@ export default async function Home({searchParams : {lang, page}} : any) {
             {/*<Facilities/>*/}
 
             { /* 블로그 */}
-            <Blog language={language} page={page || 1}/>
+            {/*<Blog language={language} page={page || 1}/>*/}
 
             { /* 문의하기 */}
             <ContactUs language={language}/>
